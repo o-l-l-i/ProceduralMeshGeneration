7992d3aad70de2c0f4dc8c89c595064a0900d737
Olli Sorjonen <olmirad@sci.fi>
2020-05-07 23:47:12 +0300

    Initial commit

---
+++ Assets/Scripts/ProceduralMeshGeneration.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+
+public class ProceduralMeshGeneration : MonoBehaviour
+{
+
+    public ComputeShader compute;
+    public Material material;
+
+    int kernel_generateMesh;
+
+    ComputeBuffer vertexBuffer;
+    ComputeBuffer normalBuffer;
+
+    ComputeBuffer drawArgsBuffer;
+
+
+    public int sideWidth = 256;
+    int quadCount;
+    int triCount;
+    int triVertCount;
+
+    public float noiseScale = 30;
+    public float displacement = 10;
+
+    static int stride = Marshal.SizeOf(typeof(Vector3));
+
+
+    void Start()
+    {
+        // Find kernel
+        kernel_generateMesh = compute.FindKernel("K_Generate");
+
+        Debug.Log("material: " + material.name);
+        Debug.Log("compute shader: " + compute.name);
+        Debug.Log("stride: " + stride);
+    }
+
+
+    // OnPostRender() seems to be broken? - Does not render even if attached to camera
+    void OnRenderObject()
+    {
+        // Calculate needed vertices etc.
+        quadCount = sideWidth * sideWidth;
+        triCount = quadCount * 2;
+        triVertCount = triCount * 3;
+
+        // If mesh configuration changes, reallocate
+        if (vertexBuffer == null || vertexBuffer.count != triVertCount)
+        {
+            Debug.Log("sideWidth: "+ sideWidth);
+            Debug.Log("quadCount: " + quadCount);
+            Debug.Log("triCount: " + triCount);
+            Debug.Log("triVertCount: " + triVertCount);
+
+            ReleaseBuffers();
+
+            ReallocateBuffers();
+
+            // Create arguments for drawing the mesh
+            // Set args array to the argument buffer
+            int[] args = new int[]{ triVertCount, 1, 0, 0 };
+            drawArgsBuffer.SetData(args);
+
+            Debug.Log("Draw args: ");
+            Debug.Log("arg[0]: " + args[0]);
+            Debug.Log("arg[1]: " + args[1]);
+            Debug.Log("arg[2]: " + args[2]);
+            Debug.Log("arg[3]: " + args[3]);
+        }
+
+        // Set variables
+        compute.SetFloat("_Time", Time.time);
+        compute.SetInt("_Size", sideWidth);
+        compute.SetFloat("_NoiseScale", noiseScale);
+        compute.SetFloat("_Displacement", displacement);
+
+        // Set buffers
+        compute.SetBuffer(kernel_generateMesh, "vertexBuffer", vertexBuffer);
+        compute.SetBuffer(kernel_generateMesh, "normalBuffer", normalBuffer);
+
+        // Generate the mesh with compute shader
+        compute.Dispatch(kernel_generateMesh, Mathf.Max(1, sideWidth/8), Mathf.Max(1, sideWidth/8), 1);
+
+        // Set the structured buffer with mesh data to the material
+        material.SetBuffer ("vertexBuffer", vertexBuffer);
+        material.SetBuffer ("normalBuffer", normalBuffer);
+
+        // Configure render pass
+        material.SetPass(0);
+
+        // Draw
+        Graphics.DrawProceduralIndirect(MeshTopology.Triangles, drawArgsBuffer, 0);
+    }
+
+
+    void ReallocateBuffers()
+    {
+        Debug.Log("Reallocating buffers.");
+        vertexBuffer   = new ComputeBuffer(triVertCount, stride, ComputeBufferType.Default);
+        normalBuffer   = new ComputeBuffer(triVertCount, stride, ComputeBufferType.Default);
+        drawArgsBuffer = new ComputeBuffer(4, sizeof(int), ComputeBufferType.IndirectArguments);
+    }
+
+
+    void ReleaseBuffers()
+    {
+        Debug.Log("Releasing buffers.");
+        if (drawArgsBuffer != null) drawArgsBuffer.Release();
+        if (vertexBuffer   != null) vertexBuffer.Release();
+        if (normalBuffer   != null) normalBuffer.Release();
+    }
+
+
+    void OnDestroy()
+    {
+        ReleaseBuffers();
+    }
+
+}
---
+++ Assets/Shaders/ProceduralMeshGenerationShader.shader
@@ -0,0 +1,125 @@
+﻿Shader "Procedural/ProceduralMeshGeneration"
+{
+
+    Properties
+    {
+        _Color   ("Diffuse Color", Color) = (1,1,1,1)
+        _SpecularStrength ("Specular Strength", float) = 100
+        _SpecularExp ("Specular exponent", float) = 32
+        _SpecularColor ("Specular Color", Color) = (1,1,1,1)
+    }
+
+    SubShader
+    {
+
+        Tags
+        {
+            "Queue"="Geometry"
+            "RenderType"="Opaque"
+        }
+
+        LOD 100
+
+        Pass
+        {
+            Tags { "LightMode" = "ForwardBase" }
+
+            CGPROGRAM
+
+            // https://forum.unity.com/threads/rwstructuredbuffer-in-vertex-shader.406592/
+            // https://forum.unity.com/threads/appendstructuredbuffer-as-output-of-compute-buffer-how-to-transfer-to-geometry-shader.664927/
+
+            #pragma target 4.5
+
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+            #include "Lighting.cginc"
+
+            #pragma multi_compile_fwdbase
+
+
+            #if SHADER_TARGET >= 45
+                uniform StructuredBuffer<float3> vertexBuffer;
+                uniform StructuredBuffer<float3> normalBuffer;
+            #endif
+
+
+            float4 _Color;
+            float _SpecularStrength;
+            float _SpecularExp;
+            float4 _SpecularColor;
+
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                // https://docs.unity3d.com/Manual/SL-ShaderSemantics.html
+                uint instanceID : SV_VertexID; // vertex ID, needs to be uint
+            };
+
+
+            struct v2f
+            {
+                float4 pos : POSITION;
+                float3 worldNormal : WORLDNORMAL;
+                float3 worldPos : TEXCOORD2;
+            };
+
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+
+                #if SHADER_TARGET >= 45
+                    float3 worldPos = vertexBuffer[v.instanceID];
+                    float4 pos = UnityObjectToClipPos(float4(worldPos, 1));
+                    float3 worldNormal = normalBuffer[v.instanceID];
+                    o.pos = pos;
+                    o.worldPos = worldPos;
+                    o.worldNormal = worldNormal;
+                #else
+                    o.pos = UnityObjectToClipPos(v.vertex);
+                #endif
+
+                return o;
+            }
+
+
+            fixed4 frag (v2f i) : COLOR
+            {
+                // Light direction
+                float3 lightDir = _WorldSpaceLightPos0;
+                float3 normal = normalize(i.worldNormal);
+
+                // NDotL
+                float NDotL = saturate(dot(normal, lightDir));
+
+                // Light color
+                float3 lightColor = _LightColor0.rgb;
+                float3 diffuse = NDotL * _Color * lightColor;
+
+                // Specular
+                float3 viewDir = normalize(_WorldSpaceCameraPos - i.worldPos);
+                float3 reflectDir = reflect(-lightDir, normal);
+                float spec = pow(max(dot(viewDir, reflectDir), 0.0), _SpecularExp);
+                float3 specular = _SpecularStrength * spec * _SpecularColor;
+
+                // Ambient
+                float3 ambient = UNITY_LIGHTMODEL_AMBIENT;
+
+                // Final composite
+                float3 shadedColor = diffuse + ambient + specular;
+                UNITY_APPLY_FOG(i.fogCoord, shadedColor);
+
+                return float4(shadedColor, 1);
+
+            }
+
+            ENDCG
+        }
+
+    }
+
+}
